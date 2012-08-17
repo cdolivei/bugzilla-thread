5af445f70f1698ea25eef69c2de4c132d3cc4475
Cesar Oliveira <me@cesaroliveira.net>
2012-08-16 20:06:18 -0400

    Replaced let with var because web pages run older versions of the js engine for some reason

--- bugzilla-thread.js
+++ bugzilla-thread.js
@@ -1,19 +1,19 @@
 (function() {
 
-let comments = document.querySelectorAll(".bz_comment > .bz_comment_text");
-let reply_regex = /^\(In reply to(?: .+? from)? comment #(\d+)\)/;
-let thread = new Array(comments.length);
-let comments_container = document.getElementById("c0").parentNode;
+var comments = document.querySelectorAll(".bz_comment > .bz_comment_text");
+var reply_regex = /^\(In reply to(?: .+? from)? comment #(\d+)\)/;
+var thread = new Array(comments.length);
+var comments_container = document.getElementById("c0").parentNode;
 
-for (let i = 0; i < comments.length; ++i)
+for (var i = 0; i < comments.length; ++i)
 {
-    let match = reply_regex.exec(comments[i].textContent);
-    let comment = comments[i].parentNode;
+    var match = reply_regex.exec(comments[i].textContent);
+    var comment = comments[i].parentNode;
     
     thread[i] = 0;
     if (match !== null)
     {
-        let reply_to = parseInt(match[1], 10);
+        var reply_to = parseInt(match[1], 10);
         thread[i] = (thread[reply_to] || 0) + 1;
         comments_container.removeChild(comment);
         comments_container.insertBefore(comment,
