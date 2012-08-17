bb11d667ed4e76dbb943807fcb76e534941b4e98
Cesar Oliveira <me@cesaroliveira.net>
2012-08-16 20:04:47 -0400

    Some statements missing a semicolon

--- bugzilla-thread.js
+++ bugzilla-thread.js
@@ -8,16 +8,16 @@ let comments_container = document.getElementById("c0").parentNode;
 for (let i = 0; i < comments.length; ++i)
 {
     let match = reply_regex.exec(comments[i].textContent);
-    let comment = comments[i].parentNode
+    let comment = comments[i].parentNode;
     
     thread[i] = 0;
     if (match !== null)
     {
         let reply_to = parseInt(match[1], 10);
         thread[i] = (thread[reply_to] || 0) + 1;
-        comments_container.removeChild(comment)
+        comments_container.removeChild(comment);
         comments_container.insertBefore(comment,
-                                        comments[reply_to].parentNode.nextElementSibling)
+                                        comments[reply_to].parentNode.nextElementSibling);
 
         comment.style.marginLeft = 
             "-moz-calc(" +
